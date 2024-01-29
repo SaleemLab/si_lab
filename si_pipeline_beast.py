f996276f176d65be4403bef8999e7839f8c06104
Di'ao Tong <61952184+atombysx@users.noreply.github.com>
2024-01-29 10:30:23 +0000

    Update si_pipeline_beast.py

--- si_pipeline_beast.py
+++ si_pipeline_beast.py
@@ -8,8 +8,9 @@ from si_process import si_process
 #grab recordings from the server to local machine (Beast)
 base_folder = '/mnt/rds01/ibn-vision/DATA/SUBJECTS/'
 mouse = 'M23038'
-dates = ['20230816','20230817']
+dates = ['20230817']
 dst_folder = "/home/lab/spikeinterface_sorting/temp_data/"
 
 for date in dates:
     si_process(base_folder, mouse, date,dst_folder)
+    
