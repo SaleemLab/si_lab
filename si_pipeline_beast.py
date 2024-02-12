fba351d4d30306c30382508fd0e3d7e847f6f177
Di'ao Tong <61952184+atombysx@users.noreply.github.com>
2024-02-12 15:03:00 +0000

    Update si_pipeline_beast.py

--- si_pipeline_beast.py
+++ si_pipeline_beast.py
@@ -4,13 +4,13 @@
 #import the necessary packages
 
 
-from si_process_one_probe import si_process_one_probe
+from si_process import si_process
 #grab recordings from the server to local machine (Beast)
 base_folder = '/mnt/rds01/ibn-vision/DATA/SUBJECTS/'
-mouse = 'M23087'
-dates = ['20231212']
+mouse = 'M23029'
+dates = ['20230707']
 dst_folder = "/home/lab/spikeinterface_sorting/temp_data/"
 job_kwargs = dict(n_jobs=20, chunk_duration='1s', progress_bar=True)
 for date in dates:
-    si_process_one_probe(base_folder, mouse, date,dst_folder,job_kwargs)
+    si_process(base_folder, mouse, date,dst_folder,job_kwargs)
 
