f4d90f0a00b2fe9595783b17f8641ca70e8bf920
Di'ao Tong <61952184+atombysx@users.noreply.github.com>
2024-02-18 23:24:43 +0000

    Update si_pipeline_godzilla.py

--- si_pipeline_godzilla.py
+++ si_pipeline_godzilla.py
@@ -8,16 +8,17 @@
 
 
 from si_process import si_process
+from si_process_one_probe import si_process_one_probe
 import os
 import subprocess
 subprocess.run('ulimit -n 4096',shell=True)
 #grab recordings from the server to local machine (Beast)
 base_folder = '/mnt/rds01/ibn-vision/DATA/SUBJECTS/'
-mouse = 'M23037'
-dates = ['20230811','20230812','20230813']
+mouse = 'M23031'
+dates = ['20230711','20230712','20230713','20230714']
 
 job_kwargs = dict(n_jobs=32, chunk_duration='1s', progress_bar=True)
 for date in dates:
     dst_folder = "/home/saleem_lab/spikeinterface_sorting/temp_data/" + date + '/'
     os.makedirs(dst_folder, exist_ok=True)
-    si_process(base_folder, mouse, date, dst_folder, job_kwargs)
+    si_process_one_probe(base_folder, mouse, date, dst_folder, job_kwargs)
