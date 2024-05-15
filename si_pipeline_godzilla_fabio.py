1b0b97150c8eb315f4832ff3bb5e196b050e6e44
FabioRibRod <51374001+FabioRibRod@users.noreply.github.com>
2024-05-15 12:22:39 +0100

    Update si_pipeline_godzilla_fabio.py

--- si_pipeline_godzilla_fabio.py
+++ si_pipeline_godzilla_fabio.py
@@ -9,8 +9,8 @@ import subprocess
 subprocess.run('ulimit -n 4096',shell=True)
 #grab recordings from the server to local machine (Beast)
 base_folder = '/mnt/rds01/ibn-vision/DATA/SUBJECTS/'
-mouse = 'M23017'
-dates = ['20230628','20230630','20230701']
+mouse = 'M24028'
+dates = ['20240426','20240427']
 
 job_kwargs = dict(n_jobs=32, chunk_duration='1s', progress_bar=True)
 for date in dates:
