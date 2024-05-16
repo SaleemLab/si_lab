a3d475b25960f969fccd6d15fcad00ae600438b8
FabioRibRod <51374001+FabioRibRod@users.noreply.github.com>
2024-05-16 10:00:42 +0100

    Update si_pipeline_godzilla_fabio.py

--- si_pipeline_godzilla_fabio.py
+++ si_pipeline_godzilla_fabio.py
@@ -6,6 +6,8 @@
 from si_process_fabio import si_process_fabio
 import os
 import subprocess
+
+from si_process_fabio_oneprobe import si_process_fabio_oneprobe
 subprocess.run('ulimit -n 4096',shell=True)
 #grab recordings from the server to local machine (Beast)
 base_folder = '/mnt/rds01/ibn-vision/DATA/SUBJECTS/'
@@ -16,4 +18,8 @@ job_kwargs = dict(n_jobs=32, chunk_duration='1s', progress_bar=True)
 for date in dates:
     dst_folder = "/home/saleem_lab/spikeinterface_sorting/temp_data/" + date + '/'
     os.makedirs(dst_folder, exist_ok=True)
-    si_process_fabio(base_folder, mouse, date, dst_folder, job_kwargs)
+    import os.path
+    if os.path.isfile(dst_folder,stream_name = 'imec1.ap'):
+        si_process_fabio(base_folder, mouse, date, dst_folder, job_kwargs)
+    else:
+        si_process_fabio_oneprobe(base_folder, mouse, date,dst_folder,job_kwargs)
