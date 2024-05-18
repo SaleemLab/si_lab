608846e35eeda3bdf952cac762e9a32fceac5b41
FabioRibRod <51374001+FabioRibRod@users.noreply.github.com>
2024-05-18 08:43:48 +0100

    Update si_process_fabio_2.py

--- si_process_fabio_2.py
+++ si_process_fabio_2.py
@@ -2,106 +2,6 @@ def si_process_fabio_2(base_folder, mouse, date,dst_folder,job_kwargs):
     
     ephys_folder = base_folder + mouse + '/ephys/' + date +'/'
     
-    # allocate destination folder and move the ephys folder on the server to Beast lab user
-    
-    from pathlib import Path
-    import platform
-    import os
-    import shutil
-    import argparse
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import spikeinterface.sorters
-    import spikeinterface.full as si
-    import  scipy.signal
-    import spikeinterface.extractors as se
-    import spikeinterface.comparison
-    import spikeinterface.exporters
-    import spikeinterface.curation
-    import spikeinterface.widgets 
-    import docker
-
-    import pandas as pd
-    probe0_segment_frames = pd.DataFrame({'segment_info':g_files,'segment start frame': probe0_start_sample_frames, 'segment end frame': probe0_end_sample_frames})
-    probe0_segment_frames.to_csv(dst_folder+'probe0/sorters/segment_frames.csv', index=False)
-    probe1_segment_frames = pd.DataFrame({'segment_info':g_files,'segment start frame': probe1_start_sample_frames, 'segment end frame': probe1_end_sample_frames})
-    probe1_segment_frames.to_csv(dst_folder+'probe1/sorters/segment_frames.csv', index=False)
-
-    probe0_we_ks4 = si.extract_waveforms(probe0_preprocessed_corrected, probe0_sorting_ks4, folder=dst_folder +'probe0/waveform/kilosort4',
-                            sparse=True, max_spikes_per_unit=500, ms_before=1.5,ms_after=2.,
-                            **job_kwargs)
-    probe1_we_ks4 = si.extract_waveforms(probe1_preprocessed_corrected, probe1_sorting_ks4, folder=dst_folder +'probe1/waveform/kilosort4',
-                            sparse=True, max_spikes_per_unit=500, ms_before=1.5,ms_after=2.,
-                            **job_kwargs)
-
-    ''' Compute quality metrics on the extracted waveforms'''
-    template_metric_probe0_ks4 = si.compute_template_metrics(probe0_we_ks4)
-    template_metric_probe1_ks4 = si.compute_template_metrics(probe1_we_ks4)
-
-    noise_levels_probe0_ks4 = si.compute_noise_levels(probe0_we_ks4)
-    noise_levels_probe1_ks4 = si.compute_noise_levels(probe1_we_ks4)
-
-    PCA_probe0_ks4 = si.compute_principal_components(probe0_we_ks4,**job_kwargs)
-    PCA_probe1_ks4 = si.compute_principal_components(probe1_we_ks4,**job_kwargs)
-
-
-    template_similarity_probe0_ks4 = si.compute_template_similarity(probe0_we_ks4)
-    template_similarity_probe1_ks4 = si.compute_template_similarity(probe1_we_ks4)
-
-    correlograms_probe0_ks4 = si.compute_correlograms(probe0_we_ks4)
-    correlograms_probe1_ks4 = si.compute_correlograms(probe1_we_ks4)
-
-    amplitudes_probe0_ks4 = si.compute_spike_amplitudes(probe0_we_ks4,**job_kwargs)
-    amplitudes_probe1_ks4 = si.compute_spike_amplitudes(probe1_we_ks4,**job_kwargs)
-    
-
-    isi_histograms_probe0_ks4 = si.compute_isi_histograms(probe0_we_ks4)
-    isi_histograms_probe1_ks4 = si.compute_isi_histograms(probe1_we_ks4)
-
-    qm_list = si.get_quality_metric_list()
-    print('The following quality metrics are computed:')
-    print(qm_list)
-    probe0_ks4_metrics = si.compute_quality_metrics(probe0_we_ks4, metric_names=qm_list,**job_kwargs)
-    probe1_ks4_metrics = si.compute_quality_metrics(probe1_we_ks4, metric_names=qm_list,**job_kwargs)
-
-    '''minor corrections to the folder path of files before moving the files to server'''
-    #process to change all the folder paths in text and .json files on Beast to the server before uploading it to the server
-    file_list = [dst_folder + "probe0_preprocessed/provenance.json",
-                dst_folder + "probe1_preprocessed/provenance.json",
-                dst_folder + "probe0/waveform/kilosort4/recording.json",
-                dst_folder + "probe0/waveform/kilosort4/sorting.json",
-                dst_folder + "probe0/sorters/kilosort4/in_container_sorting/provenance.json",
-                dst_folder + "probe0/sorters/kilosort4/in_container_sorting/si_folder.json",
-                dst_folder + "probe1/waveform/kilosort4/recording.json",
-                dst_folder + "probe1/waveform/kilosort4/sorting.json",
-                dst_folder + "probe1/sorters/kilosort4/in_container_sorting/provenance.json",
-                dst_folder + "probe1/sorters/kilosort4/in_container_sorting/si_folder.json"]
-
-    def replace_text(obj, to_replace, replace_with):
-        if isinstance(obj, dict):
-            return {k: replace_text(v, to_replace, replace_with) for k, v in obj.items()}
-        elif isinstance(obj, list):
-            return [replace_text(elem, to_replace, replace_with) for elem in obj]
-        elif isinstance(obj, str):
-            return obj.replace(to_replace, replace_with)
-        else:
-            return obj
-    import json
-
-    for files in file_list:
-        
-        # open the JSON file and load the data
-        with open(files, 'r') as f:
-            data = json.load(f)
-        
-        # replace the text
-        data = replace_text(data, dst_folder[:-1], ephys_folder[:-1])
-        
-        # write the updated data back to the JSON file
-        with open(files, 'w') as f:
-            json.dump(data, f, indent=4)
-
-    #move spikeinterface folder on Beast to the server
 
     import shutil
     import os
@@ -118,9 +18,4 @@ def si_process_fabio_2(base_folder, mouse, date,dst_folder,job_kwargs):
         # copy the folder to the destination
         shutil.copytree(dst_folder+folder, destination)
 #
-    #remove all temmp files
-#    shutil.rmtree(dst_folder)
-
-    print('All Done! Overall it took:')
-
     
