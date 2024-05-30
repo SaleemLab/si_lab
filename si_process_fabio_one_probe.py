5708fcbe9228cd1f918396ee3d24a37ae098d923
FabioRibRod <51374001+FabioRibRod@users.noreply.github.com>
2024-05-30 11:28:02 +0100

    changes

--- si_process_fabio_one_probe.py
+++ si_process_fabio_one_probe.py
@@ -38,7 +38,7 @@ def si_process_fabio_one_probe(base_folder, mouse, date,dst_folder,job_kwargs):
             source = os.path.join(ephys_folder, dirname)
             destination = os.path.join(dst_folder, dirname)
             # copy the directory to the destination folder
-            shutil.copytree(source, destination)
+            #shutil.copytree(source, destination)
     print('Start to copying files to Beast:')
     print(datetime.now() - startTime)
     ''' read spikeglx recordings and preprocess them'''
