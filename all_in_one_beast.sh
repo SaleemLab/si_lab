4befedd9535554a39a43f6e42e06c3ccf1d3092f
Di'ao Tong <61952184+atombysx@users.noreply.github.com>
2024-07-14 20:06:45 +0100

    Update all_in_one_beast.sh

--- all_in_one_beast.sh
+++ all_in_one_beast.sh
@@ -2,11 +2,11 @@
 
 # Define variables
 mouse='M24019' #mouse id
-save_date='20240701' #date of recording
-dates='20240701/20240701_0' #acquisition date and session
+save_date='20240624' #date of recording
+dates='20240624/20240624_0' #acquisition date and session
 base_folder='/home/lab/spikeinterface_sorting/temp_data/'  # Adjust this path if necessary
 no_probe=1 #number of probes you have in this session
-use_ks4=true #use kilosort4
+use_ks4=false #use kilosort4
 use_ks3=true #use kilosort3
 # Run the first Python script with inputs
 python premerging.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3
