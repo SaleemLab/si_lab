20268b0acad4b41fcc57625bcb13f9b32af66108
eabhorrocks <36156626+eabhorrocks@users.noreply.github.com>
2024-07-25 17:04:58 +0100

    edd - catgt work

--- Edd/copyFilesLocally.py
+++ Edd/copyFilesLocally.py
@@ -15,18 +15,31 @@ print('Start Time:' + startTime.strftime("%m/%d/%Y, %H:%M:%S"))
 ''' this section defines the animal and dates and fetch the recordings from the server to Beast'''
 import sys
 # The first command-line argument after the script name is the mouse identifier.
-mouse='M24019' #mouse id
-save_date='20240624' #date of recording
-dates='20240624/20240624_0' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
-base_folder='/home/lab/spikeinterface_sorting/temp_data/'  # Adjust this path if necessary
-local_folder = base_folder
-no_probe=1 #number of probes you have in this session
+#mouse='M24019' #mouse id
+#save_date='20240624' #date of recording
+#dates='20240624/20240624_0' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
+#base_folder='/home/lab/spikeinterface_sorting/temp_data/'  # Adjust this path if necessary
+#local_folder = base_folder
+#no_probe=1 #number of probes you have in this session
+
+# The first command-line argument after the script name is the mouse identifier.
+mouse = sys.argv[1]
+# All command-line arguments after `mouse` and before `save_date` are considered dates.
+dates = sys.argv[2].split(',')   # This captures all dates as a list.
+# The last command-line argument is `save_date`.
+save_date = sys.argv[3]
+local_folder = sys.argv[4]
+no_probe = sys.argv[5]
+print(mouse)
+print('acquisition folder: ',dates)
+use_ks4 = sys.argv[6].lower() in ['true', '1', 't', 'y', 'yes']
+use_ks3 = sys.argv[7].lower() in ['true', '1', 't', 'y', 'yes']
+base_folder = '/mnt/rds01/ibn-vision/DATA/SUBJECTS/'
 
 print(mouse)
 print(dates)
 print(save_date)
 
-base_folder = '/mnt/rds01/ibn-vision/DATA/SUBJECTS/'
 save_folder = local_folder+ mouse +"/" + save_date +"/"
 # get all the recordings on that day
 print(save_folder)
