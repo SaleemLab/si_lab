e4e1e6e422785f2fbcfd4c93acbda2e7336742fe
eabhorrocks <36156626+eabhorrocks@users.noreply.github.com>
2024-07-25 17:07:49 +0100

    Update all_in_one_eddev.sh

--- Edd/all_in_one_eddev.sh
+++ Edd/all_in_one_eddev.sh
@@ -10,13 +10,3 @@ use_ks4=false #use kilosort4
 use_ks3=true #use kilosort3
 # Run the first Python script with inputs
 python copyFilesLocally.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3
-
-# Run the MATLAB script for unit_match_merge_ks4_one_probe
-#matlab -nodisplay -nosplash -r "mouse='${mouse}'; date='${save_date}'; base_folder='${base_folder}';noprobe='${no_probe}'; run('unit_match_merge_ks4.m'); exit;"
-matlab -nodisplay -nosplash -r "mouse='${mouse}'; date='${save_date}'; base_folder='${base_folder}';noprobe='${no_probe}'; run('unit_match_merge_ks3.m'); exit;"
-# Run the MATLAB script for the ks3 version (assuming the script name and required adjustments)
-# matlab -nodisplay -nosplash -r "mouse='$mouse'; date=''; base_folder=''; run('your_ks3_script_name.m'); exit;"
-
-# Run the second Python script with inputs
-python merging.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3
-
