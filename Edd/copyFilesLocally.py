c355fc32e7af1b693ba0be7a4d806ba8d3527e98
eabhorrocks <36156626+eabhorrocks@users.noreply.github.com>
2024-07-26 08:53:14 +0100

    Update copyFilesLocally.py

--- Edd/copyFilesLocally.py
+++ Edd/copyFilesLocally.py
@@ -1,20 +1,13 @@
-
 # Move ephys directories from server to local drive
+
 import shutil
 from datetime import datetime
 import sys
 
 startTime = datetime.now()
 print('Start Time:' + startTime.strftime("%m/%d/%Y, %H:%M:%S"))
+
 ''' this section defines the animal and dates and fetch the recordings from the server to Beast'''
-
-# The first command-line argument after the script name is the mouse identifier.
-mouse='M24019' #mouse id
-save_date='20240626' #date of recording
-dates='20240626/20240626_0,20240626/20240626_2' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
-
-
-
 mouse = sys.argv[1] # The first command-line argument after the script name is the mouse identifier.
 dates = sys.argv[2].split(',')   # This captures all dates as a list.
 save_date = sys.argv[3]  # Defines local save location
@@ -24,7 +17,6 @@ use_ks4 = sys.argv[6].lower() in ['true', '1', 't', 'y', 'yes']
 use_ks3 = sys.argv[7].lower() in ['true', '1', 't', 'y', 'yes']
 server_path = '/mnt/rds01/ibn-vision/DATA/SUBJECTS/'
 
-
 print('Acquisition folders: ', dates)
 print('Mouse: ', mouse)
 
@@ -32,8 +24,6 @@ save_folder = local_folder + mouse + "/" + save_date + "/"
 # get all the recordings on that day
 print('Local directory location: ', save_folder)
 
-import os
-import subprocess
 
 #grab recordings from the server to local machine
 print(dates)
