5d3e082157aed5cf3774114317572dd4a17776ac
eabhorrocks <36156626+eabhorrocks@users.noreply.github.com>
2024-08-22 14:26:38 +0100

    edd up

--- Edd/all_in_one_eddev.sh
+++ Edd/all_in_one_eddev.sh
@@ -2,8 +2,8 @@
 
 # Define variables
 mouse='M24019' #mouse id
-save_date='20240718' #date of recording
-dates='20240718/20240718_0' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
+save_date='20240719' #date of recording
+dates='20240719/20240719_0,20240719/20240719_2' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
 base_folder='/home/lab/spikeinterface_sorting/temp_data/'  # Adjust this path if necessary
 
 no_probe=1 #number of probes you have in this session
@@ -20,14 +20,13 @@ use_ks3=false #use kilosort3
 #pre-processing as separate script
 #python preprocessProbes.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3
 
-# (load pre-processed data)
 #python runSpikeSorting.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3
 
-#matlab -nosplash -nodisplay -nodesktop -r "mouse='${mouse}'; date='${save_date}'; base_folder='${base_folder}';no_probe='${no_probe}';dates='${dates}'; run('unit_match_merge_ks4_eddit.m'); exit;"
+matlab -nosplash -nodisplay -nodesktop -r "mouse='${mouse}'; date='${save_date}'; base_folder='${base_folder}';no_probe='${no_probe}';dates='${dates}'; run('unit_match_merge_ks4_eddit2.m'); exit;"
 
 #python mergeAndCalculateQualityMetrics.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3
 
-python uploadToServer.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3
+#python uploadToServer.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3
 
 # python uplaod to server
 
