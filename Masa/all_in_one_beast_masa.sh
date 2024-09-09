13ba5e1b4476b9a189bdb0648f6800c7fd373ba9
masahirotaki <masahirotakigaw@gmail.com>
2024-09-09 17:40:48 +0100

    Update all_in_one_beast_masa.sh

--- Masa/all_in_one_beast_masa.sh
+++ Masa/all_in_one_beast_masa.sh
@@ -11,13 +11,13 @@ use_ks3=true #use kilosort3
 # g_files_to_ignore input currently not working for some reasons.... but it works if it is inside
 g_files_to_ignore=['tcat'] # e.g. g_files_to_ignore=['tcat','0_g6','0_g7','0_g8','0_g9']
 # Run the first Python script with inputs
-python premerging_masa_temp.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3 $g_files_to_ignore
-python premerging_masa_temp2.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3 $g_files_to_ignore
-#python premerging_masa.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3 $g_files_to_ignore
+#python premerging_masa_temp.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3 $g_files_to_ignore
+#python premerging_masa_temp2.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3 $g_files_to_ignore
+python premerging_masa.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3 $g_files_to_ignore
 
 #python premerging.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3
 # Run the MATLAB script for unit_match_merge_ks4_one_probe
-#matlab -nodisplay -nosplash -r "mouse='${mouse}'; date='${save_date}'; base_folder='${base_folder}';noprobe='${no_probe}'; run('unit_match_merge_ks4.m'); exit;"
+matlab -nodisplay -nosplash -r "mouse='${mouse}'; date='${save_date}'; base_folder='${base_folder}';noprobe='${no_probe}'; run('unit_match_merge_ks4.m'); exit;"
 matlab -nodisplay -nosplash -r "mouse='${mouse}'; date='${save_date}'; base_folder='${base_folder}';noprobe='${no_probe}'; run('unit_match_merge_ks3.m'); exit;"
 # Run the MATLAB script for the ks3 version (assuming the script name and required adjustments)
 # matlab -nodisplay -nosplash -r "mouse='$mouse'; date=''; base_folder=''; run('your_ks3_script_name.m'); exit;"
