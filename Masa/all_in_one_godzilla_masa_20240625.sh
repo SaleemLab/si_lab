a3f99a595a940d5748893bad61ee73d32e3ffc26
masahirotaki <masahirotakigaw@gmail.com>
2024-09-16 18:35:39 +0100

    update

--- Masa/all_in_one_godzilla_masa_20240625.sh
+++ Masa/all_in_one_godzilla_masa_20240625.sh
@@ -12,7 +12,7 @@ export KILOSORT3_PATH=/home/saleem_lab/Kilosort #path to kilosort3
 # g_files_to_ignore input currently not working for some reasons.... but it works if it is inside
 g_files_to_ignore=['tcat'] # e.g. g_files_to_ignore=['tcat','0_g6','0_g7','0_g8','0_g9']
 # Run the first Python script with inputs
-python premerging_masa_local.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3 $g_files_to_ignore
+python premerging_masa_local_ks3.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3 $g_files_to_ignore
 
 #python premerging_masa_temp_godzilla.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3 $g_files_to_ignore
 #python premerging_masa_temp2_godzilla.py $mouse $dates $save_date $base_folder $no_probe $use_ks4 $use_ks3 $g_files_to_ignore
