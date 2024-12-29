37547d7df2ff4df9d5c7b67a8bf19e3ec6d3c738
masahirotaki <masahirotakigaw@gmail.com>
2024-12-29 02:04:01 +0000

    Update merging_kingkong.py

--- merging_kingkong.py
+++ merging_kingkong.py
@@ -201,8 +201,8 @@ for probe in range(int(no_probe)):
 
     ##
     #
-   folders_to_move = ['probe'+str(probe),
-               'probe'+str(probe)+'_motion/']
+    folders_to_move = ['probe'+str(probe),
+                'probe'+str(probe)+'_motion/']
 
     for folder in folders_to_move:
         # construct the destination path
