79cff5e28a475cbc9f96056cd2612c1eb3355d7e
Jork de Kok <jork.dekok@live.nl>
2025-01-16 17:16:02 +0000

    Update all_in_one_beast_Jork.sh

--- all_in_one_beast_Jork.sh
+++ all_in_one_beast_Jork.sh
@@ -11,8 +11,9 @@ use_ks4=true #use kilosort4
 use_ks3=true #use kilosort3
 server_folder='/home/lab/rds01/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
 g_files_to_ignore='[[],[0,1]]' #files to ignore for each probe
+#g_files_to_ignore='[[0,1],[]]' #files to ignore for each probe
 # Run the first Python script with inputs
-python premerging_zarr.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
+python premerging_zarr.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
 
 
 # Run the MATLAB script for unit_match_merge_ks4_one_probe
