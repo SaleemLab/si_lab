682ab052bfc7fdb685943c80ceb292b2f9141772
Jork de Kok <jork.dekok@live.nl>
2025-01-19 13:36:51 +0000

    Update all_in_one_beast_Jork.sh

--- all_in_one_beast_Jork.sh
+++ all_in_one_beast_Jork.sh
@@ -10,8 +10,8 @@ no_probe=1 #number of probes you have in this session
 use_ks4=true #use kilosort4 
 use_ks3=true #use kilosort3
 server_folder='/mnt/rds01/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
-g_files_to_ignore='[[],[0,1]]' #files to ignore for each probe
-#g_files_to_ignore='[[0,1],[]]' #files to ignore for each probe
+#g_files_to_ignore='[[],[0,1]]' #files to ignore for each probe
+g_files_to_ignore='[[0,1],[]]' #files to ignore for each probe
 # Run the first Python script with inputs
 python premerging_zarr_temp.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
 
