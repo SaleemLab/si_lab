70f8624b1c6bd22d9aa1aa6cdc2847f017056293
Jork de Kok <jork.dekok@live.nl>
2025-01-22 12:16:25 +0000

    Update all_in_one_beast_Jork.sh

--- all_in_one_beast_Jork.sh
+++ all_in_one_beast_Jork.sh
@@ -9,11 +9,11 @@ base_folder='/home/lab/spikeinterface_sorting/temp_data/'  # local folder of god
 no_probe=1 #number of probes you have in this session
 use_ks4=true #use kilosort4 
 use_ks3=true #use kilosort3
-server_folder='/mnt/rds01/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
-#g_files_to_ignore='[[],[0,1]]' #files to ignore for each probe
+server_folder='/home/lab/bendor/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
+#g_files_to_ignore='[]' #files to ignore for each probe
 #g_files_to_ignore='[[0,1],[]]' #files to ignore for each probe
 # Run the first Python script with inputs
-python premerging_zarr.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
+python premerging_zarr.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder 
 
 
 # Run the MATLAB script for unit_match_merge_ks4_one_probe
