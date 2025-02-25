295c8f12caf109013477edc45f2548601269064b
masahirotaki <masahirotakigaw@gmail.com>
2025-02-25 19:01:04 +0000

    Update all_in_one_kingkong.sh

--- all_in_one_kingkong.sh
+++ all_in_one_kingkong.sh
@@ -70,30 +70,6 @@ python upload_kingkong_probe0.py $mouse $save_date $base_folder $no_probe $use_k
 
 # Define variables
 mouse='M24018' #mouse id
-save_date='20240708' #date of recording
-#dates='20241220/20241220_0,20241220/20241220_1' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
-base_folder='/home/masa/spikesorting_temp_data/'  # local folder of godzilla
-no_probe=2 #number of probes you have in this session
-use_ks4=true #use kilosort4 
-use_ks3=true #use kilosort3
-server_folder='/home/masa/rds01/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
-server_folder='/saleem/ibn-vision/DATA/SUBJECTS/'
-g_files_to_ignore='[[6,7,8,9]]' #files to ignore for each probe
-
-# Run the first Python script with inputs
-python download_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
-python premerging_zarr_kingkong_probe0.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder 
-python premerging_zarr_kingkong_probe1.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
-#python premerging_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
-#python premerging_zarr.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
-
-python upload_kingkong_probe1.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
-python upload_kingkong_probe0.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
-
-
-
-# Define variables
-mouse='M24018' #mouse id
 save_date='20240711' #date of recording
 #dates='20241220/20241220_0,20241220/20241220_1' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
 base_folder='/home/masa/spikesorting_temp_data/'  # local folder of godzilla
@@ -113,6 +89,30 @@ python premerging_zarr_kingkong_probe1.py $mouse $save_date $base_folder $no_pro
 
 python upload_kingkong_probe1.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
 python upload_kingkong_probe0.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
+
+# Define variables
+mouse='M24018' #mouse id
+save_date='20240710' #date of recording
+#dates='20241220/20241220_0,20241220/20241220_1' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
+base_folder='/home/masa/spikesorting_temp_data/'  # local folder of godzilla
+no_probe=2 #number of probes you have in this session
+use_ks4=true #use kilosort4 
+use_ks3=true #use kilosort3
+server_folder='/home/masa/rds01/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
+server_folder='/saleem/ibn-vision/DATA/SUBJECTS/'
+g_files_to_ignore='[[6,7,8,9]]' #files to ignore for each probe
+
+# Run the first Python script with inputs
+python download_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
+python premerging_zarr_kingkong_probe0.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder 
+python premerging_zarr_kingkong_probe1.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
+#python premerging_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
+#python premerging_zarr.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
+
+python upload_kingkong_probe1.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
+python upload_kingkong_probe0.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
+
+
 
 # Define variables
 mouse='M24062' #mouse id
@@ -192,12 +192,12 @@ use_ks4=true #use kilosort4
 use_ks3=true #use kilosort3
 server_folder='/home/masa/rds01/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
 server_folder='/saleem/ibn-vision/DATA/SUBJECTS/'
-g_files_to_ignore='[[],[0,1,2,3]]' #files to ignore for each probe
+g_files_to_ignore='[[],[],[0,1,2,3,4,5]]' #files to ignore for each probe
 
 # Run the first Python script with inputs
 python download_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
-python premerging_zarr_kingkong_probe0.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder 
-python premerging_zarr_kingkong_probe1.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder 
+python premerging_zarr_kingkong_probe0.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
+python premerging_zarr_kingkong_probe1.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
 #python premerging_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
 #python premerging_zarr.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
 
@@ -239,7 +239,7 @@ use_ks4=true #use kilosort4
 use_ks3=true #use kilosort3
 server_folder='/home/masa/rds01/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
 server_folder='/saleem/ibn-vision/DATA/SUBJECTS/'
-g_files_to_ignore='[[],[0,1,2,3]]' #files to ignore for each probe
+g_files_to_ignore='[[],[0,1,2,3,4]]' #files to ignore for each probe
 
 # Run the first Python script with inputs
 python download_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
