c23549f55c1b8a28552595c4aa42b0edbdaa6709
EleanorBenoit <eleanor_rosalind@yahoo.com>
2025-04-08 11:06:22 +0100

    Updated all_in_one_kingkong_Ellie.sh for 4th Feb recording

--- all_in_one_kingkong_Ellie.sh
+++ all_in_one_kingkong_Ellie.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
 
+#!/bin/bash
 mouse='M00013' #mouse id
-save_date='20250203' #date of recording
+save_date='20240204' #date of recording
 #dates='20241220/20241220_0,20241220/20241220_1' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
-base_folder='/home/masa/spikesorting_temp_data/'  # local folder of godzilla
+base_folder='/home/masa/spikesorting_temp_data/'  # local folder of kingkong
 no_probe=1 #number of probes you have in this session
 use_ks4=true #use kilosort4 
 use_ks3=true #use kilosort3
-server_folder='/home/masa/rds01/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
-server_folder='/bendor/Ellie/DATA/SUBJECTS/'
+server_folder='/bendor/Ellie/DATA/SUBJECTS'
 g_files_to_ignore='[[],[0,1,2,3]]' #files to ignore for each probe
 
 # Run the first Python script with inputs
