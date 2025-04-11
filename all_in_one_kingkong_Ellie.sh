263c23f1d4755c0e2919cdc1f82e1cf7757a0f6d
EleanorBenoit <eleanor_rosalind@yahoo.com>
2025-04-11 14:04:58 +0100

    Update all_in_one_kingkong_Ellie.sh

--- all_in_one_kingkong_Ellie.sh
+++ all_in_one_kingkong_Ellie.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 mouse='M00013' #mouse id
-save_date='20250205' #date of recording
+save_date='20250204' #date of recording
 #dates='20241220/20241220_0,20241220/20241220_1' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
 base_folder='/home/masa/spikesorting_temp_data/'  # local folder of kingkong
 no_probe=1 #number of probes you have in this session
@@ -12,7 +12,7 @@ g_files_to_ignore='[[],[0,1,2,3]]' #files to ignore for each probe
 
 # Run the first Python script with inputs
 # comment out download_zarr_kingkong.py if this step completed successfully (copying of zarr files to Bendor24 temp folder)
-python download_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
+#python download_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
 python premerging_zarr_kingkong_probe0.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder 
 #python premerging_zarr_kingkong_probe1.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder 
 #python premerging_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder $g_files_to_ignore
