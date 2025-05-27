08b13f0bd4942fbb2d14597fc9831cae93137d0e
masahirotaki <masahirotakigaw@gmail.com>
2025-05-27 10:05:35 +0100

    Update all_in_one_kingkong.sh

--- all_in_one_kingkong.sh
+++ all_in_one_kingkong.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-mouse='M24017' #mouse id
-save_date='20240614' #date of recording
+# Define variables
+mouse='M24018' #mouse id
+save_date='20240723' #date of recording
 #dates='20241220/20241220_0,20241220/20241220_1' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
 base_folder='/home/masa/spikesorting_temp_data/'  # local folder of godzilla
 no_probe=2 #number of probes you have in this session
 use_ks4=true #use kilosort4 
 use_ks3=true #use kilosort3
-#server_folder='/home/masa/rds01/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
-#server_folder='/saleem/ibn-vision/DATA/SUBJECTS/'
 server_folder='/run/user/1004/gvfs/smb-share:server=rdp.arc.ucl.ac.uk,share=ritd-ag-project-rd01ie-asale69/ibn-vision/DATA/SUBJECTS/'
 
 g_files_to_ignore='[[],[0,1,2,3]]' #files to ignore for each probe
@@ -25,14 +24,17 @@ python spikesorting_zarr_kingkong_probe0.py $mouse $save_date $base_folder $no_p
 python upload_kingkong_probe1.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
 python upload_kingkong_probe0.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
 
+
 # Define variables
-mouse='M24018' #mouse id
-save_date='20240723' #date of recording
+mouse='M24017' #mouse id
+save_date='20240614' #date of recording
 #dates='20241220/20241220_0,20241220/20241220_1' #acquisition date and session e.g. dates='20240624/20240624_0,20240624/20240624_1'
 base_folder='/home/masa/spikesorting_temp_data/'  # local folder of godzilla
 no_probe=2 #number of probes you have in this session
 use_ks4=true #use kilosort4 
 use_ks3=true #use kilosort3
+#server_folder='/home/masa/rds01/ibn-vision/DATA/SUBJECTS/' #server folder where the data is stored
+#server_folder='/saleem/ibn-vision/DATA/SUBJECTS/'
 server_folder='/run/user/1004/gvfs/smb-share:server=rdp.arc.ucl.ac.uk,share=ritd-ag-project-rd01ie-asale69/ibn-vision/DATA/SUBJECTS/'
 
 g_files_to_ignore='[[],[0,1,2,3]]' #files to ignore for each probe
@@ -162,7 +164,7 @@ use_ks4=true #use kilosort4
 use_ks3=true #use kilosort3
 server_folder='/run/user/1004/gvfs/smb-share:server=rdp.arc.ucl.ac.uk,share=ritd-ag-project-rd01ie-asale69/ibn-vision/DATA/SUBJECTS/'
 
-g_files_to_ignore='[],[],[],[0,1,2,3]]' #files to ignore for each probe
+g_files_to_ignore='[[],[],[],[0,1,2,3]]' #files to ignore for each probe
 
 #### 1. comment out download_zarr_kingkong.py if this step completed successfully (copying of zarr files to Bendor24 temp folder)
 python download_zarr_kingkong.py $mouse $save_date $base_folder $no_probe $use_ks4 $use_ks3 $server_folder
